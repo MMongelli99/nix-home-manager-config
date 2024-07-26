38bbbfbad7820c6ad747d8d0f437687bc9cf6de0
Michael Mongelli <mmongelli99@gmail.com>
2024-07-26 15:55:32 -0400

    several changes:   - added dotfile .ghc/ghci.conf to home.file   - nixvim config     * replaced lightline with lualine     * added bufferline     * added nvim-scrollbar     * added codeium-vim AI autocompletion   - added a couple of shell aliases   - global git config

--- home.nix
+++ home.nix
@@ -42,12 +42,9 @@
     zsh-powerlevel10k
     meslo-lgs-nf
     ripgrep # needed for telescope.nvim
-
-    ghc
     warp-terminal
     neovide
     cached-nix-shell
-    nixfmt-rfc-style
 
     # TODO: to try in the future
     # virtualbox            # not available on MacOS
@@ -82,6 +79,19 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+
+    ".ghc/ghci.conf".text = ''
+      -- multiline mode
+      :set +m
+      -- prompt for each line input
+      :set prompt  "𝝺 ❯ "
+      -- how to add color: https://wiki.haskell.org/GHCi_in_colour
+      :set prompt "\ESC[35m\STX𝝺 ❯ \ESC[m\STX"
+      -- prompt for each line of multiline input
+      :set prompt-cont "... "
+      -- add color to multiline continuation
+      :set prompt-cont "\ESC[35m\STX... \ESC[m\STX" 
+    '';
   };
 
   # Home Manager can also manage your environment variables through
@@ -109,23 +119,31 @@
   programs.home-manager.enable = true;
 
   programs.nixvim = {
-    enable       = true; 
+    enable       = true;
     viAlias      = true;
     vimAlias     = true;
     vimdiffAlias = true;
 
     opts = {
       number = true;         # Show line numbers
-      # relativenumber = true; # Show relative line numbers
-
+      relativenumber = true; # Show relative line numbers
       shiftwidth = 2;        # Tab width should be 2
     };
      
     # Nixvim modules colorschemes: https://github.com/nix-community/nixvim/tree/main/plugins/colorschemes
     # colorschemes.vscode.enable = true;
-    # plugins.lightline.enable = true;
 
     plugins = {
+
+      # navic = {
+      #   enable = true;
+      #   lsp.autoAttach = true;
+      # }; 
+
+      # neoscroll = {
+      #   enable = true;
+      #   settings.easing_function = "linear";
+      # };
 
       lualine = {
         enable = true;
@@ -140,43 +158,11 @@
         # };
       };
 
-      lsp = {
-        enable = true;
+      bufferline.enable = true;
 
-        keymaps = {
-          silent = true;
-          # diagnostic = {
-          #   # Navigate in diagnostics
-          #   "<leader>k" = "goto_prev";
-          #   "<leader>j" = "goto_next";
-          # };
-
-          lspBuf = {
-            gd = "definition";
-            gD = "references";
-            gt = "type_definition";
-            gi = "implementation";
-            # K = "hover";
-            # "<F2>" = "rename";
-          };
-        };
-
-	# Nixvim LSP modules: https://github.com/nix-community/nixvim/blob/a5e9dbdef1530a76056db12387d489a68eea6f80/plugins/lsp/language-servers/default.nix#L57
-        servers = {
-	  pylsp.enable       = true; # Python
-	  nil-ls.enable      = true; # Nix
-	  bashls.enable      = true; # Bash
-	  html.enable        = true; # HTML
-	  cssls.enable       = true; # CSS
-	  tailwindcss.enable = true; # TailwindCSS
-	  jsonls.enable      = true; # JSON
-	  htmx.enable        = true; # HTMX
-	  sqls.enable        = true; # SQL
-	  lua-ls.enable      = true; # Lua
-	  marksman.enable    = true; # Markdown
-	  hls.enable         = true; # Haskell
-        };
-      };
+      diffview.enable = true;
+      
+      codeium-vim.enable = true;
 
       neo-tree = {
 	enable = true;
@@ -191,6 +177,16 @@
 	};
       };
 
+      /* fzf-lua = {
+	enable = true; 
+	keymaps = {
+	  "<leader>ff" = "find_files";
+	  "<leader>fg" = "live_grep";
+	  "<leader>fb" = "buffers";
+	  "<leader>fh" = "help";
+	};
+      }; */
+
       telescope = {
         enable = true;
         settings = {
@@ -199,6 +195,12 @@
           };
         };
         keymaps = {
+	  "<leader>fs" = {
+	    action = "grep_string";
+	    options = {
+	      desc = "Find String";
+	    };
+	  };
           "<leader>ff" = {
             action = "find_files";
             options = {
@@ -268,6 +270,43 @@
 	};
       };
 
+      lsp = {
+        enable = true;
+
+        keymaps = {
+          silent = true;
+          # diagnostic = {
+          #   # Navigate in diagnostics
+          #   "<leader>k" = "goto_prev";
+          #   "<leader>j" = "goto_next";
+          # };
+
+          lspBuf = {
+            gd = "definition";
+            gD = "references";
+            gt = "type_definition";
+            gi = "implementation";
+            # K = "hover";
+            # "<F2>" = "rename";
+          };
+        };
+
+	# Nixvim LSP modules: https://github.com/nix-community/nixvim/blob/a5e9dbdef1530a76056db12387d489a68eea6f80/plugins/lsp/language-servers/default.nix#L57
+        servers = {
+	  pylsp.enable       = true; # Python
+	  nil-ls.enable      = true; # Nix
+	  bashls.enable      = true; # Bash
+	  html.enable        = true; # HTML
+	  cssls.enable       = true; # CSS
+	  tailwindcss.enable = true; # TailwindCSS
+	  jsonls.enable      = true; # JSON
+	  htmx.enable        = true; # HTMX
+	  sqls.enable        = true; # SQL
+	  lua-ls.enable      = true; # Lua
+	  marksman.enable    = true; # Markdown
+	  hls.enable         = true; # Haskell
+        };
+      };
 
     };
     
@@ -283,32 +322,45 @@
         options.desc = "Toggle NeoTree";
       }
     ];
+
+    # See NixVim GitHub - nixvim/docs/user-guide/faq.md
+    # This is straightforward too, you can add the following to `extraPlugins` for a plugin hosted on GitHub:
+    # (pkgs.vimUtils.buildVimPlugin {
+    #   name = "my-plugin";			# plugin name
+    #   version = "<version>";  		# plugin version (optional)
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "<owner>";         		# username of repo owner
+    #     repo  = "<repo>";          		# name of repo
+    #     rev   = "<commit hash>";   		# full SHA hash of commit
+    #     hash  = "<nix NAR hash>";             # run `nix-prefetch-url --unpack <url for zip file of repo at specified commit>`
+    #   };
+    #   meta.homepage = "<URL for docs/repo>";  # link to project docs / GitHub repo (optional)
+    # })
     
-    # vimPlugin colorschemes: https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/applications/editors/vim/plugins/generated.nix
+    # nixpkgs vim plugins: https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/applications/editors/vim/plugins/generated.nix
     extraPlugins = with pkgs.vimPlugins; [ 
+
       neo-tree-nvim
       nvim-notify
       markview-nvim
-      ## colorschemes ##
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "nvim-scrollbar";
+	version = "2024-06-03";
+        src = pkgs.fetchFromGitHub {
+          owner   = "petertriho";
+          repo    = "nvim-scrollbar";
+          rev     = "d09f14aa16c9f2748e77008f9da7b1f76e4e7b85";
+          sha256  = "1zm0xaph29hk2dw4rpmpz67nxxbr39f67zfil5gwyzk3d87q56k3";
+        };
+	meta.homepage = "https://github.com/petertriho/nvim-scrollbar/";
+      })
+
+      # colorschemes #
+
       pure-lua          # moonlight
       vim-code-dark     # codedark
       vim-monokai-tasty # vim-monokai-tasty
-      poimandres-nvim   # poimandres
-      
-      # See GitHub - nixvim/docs/user-guide/faq.md
-      # This is straightforward too, you can add the following to `extraPlugins` for a plugin hosted on GitHub:
-      # (pkgs.vimUtils.buildVimPlugin {
-      #   name = "my-plugin";
-      #   version = "<version>";  # optional date of commit (YYYY-MM-DD), semantic versioning provided for release, or however you prefer to version it
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "<owner>";         # username of repo owner
-      #     repo  = "<repo>";          # name of repo
-      #     rev   = "<commit hash>";   # commit hash
-      #     hash  = "<nix NAR hash>";  # run `nix-prefetch-url --unpack <URL for .zip of repo at specified commit>`
-      #   };
-      #   meta.homepage = "<URL for docs/repo>";  # optional link to project docs or GitHub repo
-      # })
-
+      poimandres-nvim   # poimandres 
       (pkgs.vimUtils.buildVimPlugin {
         name = "vague.nvim";
 	version = "1.2.0";
@@ -323,9 +375,25 @@
 
     ];
 
-    colorscheme = "vim-monokai-tasty"; 
-    plugins.lightline.enable = false; # lightline only inherits colorschemes from Nixvim modules, lualine is a better alternative
+    colorscheme = "vim-monokai-tasty";  
 
+    extraConfigLua = ''
+      require("scrollbar").setup({
+        throttle_ms = 0,
+	handle = {
+	    blend = 75, -- transparency %
+	    color = "#ffffff",
+	},
+        marks = {
+	  Cursor = {
+	    text = " ",
+	    -- text = "◼",
+	    -- color = "#aaaaaa",
+	  },
+	},
+      })
+    '';
+     
   };
   
   # basically copy the whole nvchad that is fetched from github to ~/.config/nvim
@@ -334,16 +402,23 @@
   # };
 
   programs.zsh = {
+    
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     autocd = true;
     syntaxHighlighting.enable = true;
 
-    initExtra = ''
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-      source ~/.p10k.zsh
-    '';
+    shellAliases = {
+      switch   = "home-manager switch";
+      git-tree = "git log --graph --decorate --oneline $(git rev-list -g --all)";
+    };
+
+    # oh-my-zsh = {
+    #   enable = true;
+    #   plugins = [ "git" "systemd" "rsync" "kubectl" ];
+    #   theme = "terminalparty";
+    # }; 
     
     plugins = [
       {
@@ -352,10 +427,22 @@
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
     ];
-    
-    shellAliases = {
-      # executing iTerm2 from command line has problems when using Nix's default iTerm2 path
-      iTerm2 = "exec ~/Applications/Home Manager Apps/iTerm2.app/Contents/MacOS/iTerm2";
+
+    initExtra = ''
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      source ~/.p10k.zsh
+    ''; 
+
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Michael Mongelli";
+    userEmail = "mmongelli99@gmail.com";
+    ignores = [ ".DS_Store" ];
+    extraConfig = {
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
     };
   };
 
