8c277b883a3582d35ddd2cac847dd2acae47d4ec
Michael Mongelli <mmongelli99@gmail.com>
2024-12-05 12:54:08 -0500

    added extensions to vscode config

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -7,13 +7,29 @@
       visualstudioexptteam.vscodeintellicode # AI assisted suggestions
 			tim-koehler.helm-intellisense # type-inferenced suggestions
 			jnoortheen.nix-ide # nix language support
+			mkhl.direnv
 			# b4dm4n.vscode-nixpkgs-fmt
 			# brettm12345.nixfmt-vscode
-			wholroyd.jinja
-			mkhl.direnv
+
+			# Python
+			wholroyd.jinja # jinja syntax highlighting
+			ms-python.python # python language support, intellisense, linting, etc.
+			ms-python.vscode-pylance # needed for Intellicode
+			matangover.mypy # cross-file type checking, `pip install dmypy-ls` with this extension
 			# phind.phind
-			# asvetliakov.vscode-neovim # loaded with errors about lua
+			asvetliakov.vscode-neovim # loaded with errors about lua
 			# vscodevim.vim # interfering with prettier and autosuggestions
+
+			waderyan.gitblame
+
+			ms-vscode-remote.remote-ssh
+			ms-vscode-remote.remote-ssh-edit
+
+			nonylene.dark-molokai-theme
+
+			tamasfe.even-better-toml
+
+			# wakatime.vscode-wakatime
 		];
 	};
 }
